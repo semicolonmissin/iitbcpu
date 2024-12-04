83a46f909b36653b2708a0b7165f2c6c415513c0
Srivathsa <153110778+semicolonmissin@users.noreply.github.com>
2024-12-04 20:41:45 +0530

    Update Sign_ext.vhd

--- Sign_ext.vhd
+++ Sign_ext.vhd
@@ -22,6 +22,26 @@ library ieee;
 use ieee.std_logic_1164.all;
 use IEEE.NUMERIC_STD.ALL;
 
+entity SE_6to15 is
+    port (
+        A: in std_logic_vector(5 downto 0);
+        C: out std_logic_vector(14 downto 0)
+    );
+end entity;
+
+architecture struct of SE_6to15 is
+begin
+
+process(A)
+begin 
+	C <= std_logic_vector(to_signed(to_integer(signed(A)),15));
+end process;
+end struct;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use IEEE.NUMERIC_STD.ALL;
+
 entity SE_9to16 is
     port (
         A: in std_logic_vector(8 downto 0);
