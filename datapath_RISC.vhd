108b8475a69eb2f77a9241bef51d2e27d58a089d
DheerajNenavath <148033614+DheerajNenavath@users.noreply.github.com>
2024-12-04 22:56:01 +0530

    Update datapath_RISC.vhd final datapath without m14

--- datapath_RISC.vhd
+++ datapath_RISC.vhd
@@ -5,16 +5,16 @@ entity data_path is
     port (
         state : in integer;
         alu_z : in std_logic;
-        m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, pc_w, mw, ir_w, rf_w, t1_w, t2_w, t3_w, z_en : out std_logic;
+        m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, pc_w, mw, ir_w, rf_w, t1_w, t2_w, t3_w, z_en : out std_logic;
 	op : out std_logic_vector(2 downto 0)
     );
 end entity;
 
 architecture struct of data_path is
     signal m1_var, m2_var, m3_var, m4_var, m5_var, m6_var, m7_var, m8_var, m9_var, m10_var,
-           m11_var, m12_var, m13_var, m14_var, pc_w_var, mw_var, ir_w_var, rf_w_var,
+           m11_var, m12_var, m13_var, pc_w_var, mw_var, ir_w_var, rf_w_var,
            t1_w_var, t2_w_var, t3_w_var, z_en_var : std_logic;
-signal op_var : std_logic_vector(2 downto 0);
+    signal op_var : std_logic_vector(2 downto 0);
 
 begin
     process(state)
@@ -33,7 +33,6 @@ begin
         m11_var   <= '0';
         m12_var   <= '0';
         m13_var   <= '0';
-        m14_var   <= '0';
         pc_w_var  <= '0';
         mw_var    <= '0';
         ir_w_var  <= '0';
@@ -51,6 +50,7 @@ begin
             when 1 =>
                 m13_var <= '1';
                 pc_w_var <= '1';
+		op_var <= '000';
 
             when 2 =>
                 t1_w_var <= '1';
@@ -80,7 +80,7 @@ begin
                 m8_var <= '1';
                 m11_var <= '1';
                 t3_w_var <= '1';
-		op_var <= '101;
+		op_var <= '101';
 
             when 8 =>
                 m3_var <= '1';
@@ -105,18 +105,12 @@ begin
                 m13_var <= '1';
 		op_var <= '110';
 
-            when 12 =>
-                m14_var <= '1';
-                t2_w_var <= '1';
-
             when 13 =>
                 rf_w_var <= '1';
-                t3_w_var <= '1';
 		m6_var <= '1';
 
             when 14 =>
                 rf_w_var <= '1';
-                t3_w_var <= '1';
 		m5_var <= '1';
 
             when 15 =>
@@ -151,13 +145,13 @@ begin
             when 22 =>
                 m8_var <= '1';
                 m11_var <= '1';
-		op_var <= '100';
+		op__var <= '100';
 
             when 23 =>
                 m9_var <= '1';
 
             when 24 =>
-                m14_var <= alu_z;
+                m13_var <= alu_z;
                 z_en_var <= '1';
 		pc_w_var <= '1';
 
@@ -194,7 +188,7 @@ begin
                 t1_w_var  <= '0';
                 t2_w_var  <= '0';
                 t3_w_var  <= '0';
-		z_en_var  <= '0';
+					 z_en_var  <= '0';
 		op_var <= '000';
         end case;
 
@@ -214,7 +208,6 @@ begin
     m11 <= m11_var;
     m12 <= m12_var;
     m13 <= m13_var;
-    m14 <= m14_var;
     pc_w <= pc_w_var;
     mw <= mw_var;
     ir_w <= ir_w_var;
